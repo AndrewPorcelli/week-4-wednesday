8b38f98733e009666d7ebe13c3f60678db37c69f
Andrew Porcelli <andy.porcelli1@gmail.com>
2022-06-15 17:22:00 -0600

    added a comment on how to do it withouth the query search

--- swapi/main.js
+++ swapi/main.js
@@ -7,14 +7,14 @@ function clickBtn() {
   // console.log('button clicked');
   axios.get("https://swapi.dev/api/planets/?search=Alderaan").then((res) => {
     console.log(res.data);
-    const residents = res.data.results[0].residents;
+    const residents = res.data.results[0].residents; // could do res.data.residents if the get was /planets/2
     for (let i = 0; i < residents.length; i++) {
       console.log(residents[i]);
       axios.get(residents[i]).then((res) => {
         console.log(res.data);
         let b = document.createElement("h2");
         b.textContent = res.data.name;
-        console.log(res.data.name)
+        console.log(res.data.name);
         body.append(b);
       });
     }
